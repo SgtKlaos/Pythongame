a26d16bb28bd86f55a8c7ee14d1365fa294a7f92
Jesse Arnold <jarnold25@my.whitworth.edu>
2023-12-03 22:26:39 -0800

    Fixed bug: p1 dropping out before W, +getch 4 keys

--- ClientSide.py
+++ ClientSide.py
@@ -1,25 +1,27 @@
 import socket
 
+bufferSize = 1024; # maximum size of transmission to listen for
+
 # Create a socket object
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Connect to the server
-server_address = ('localhost', 12345)
+server_address = ('localhost', 50000)
 client_socket.connect(server_address)
 print('Connected to the server')
 
 # Receive the game start message
-start_message = client_socket.recv(1024).decode('utf-8')
+start_message = client_socket.recv(bufferSize).decode('utf-8')
 print(start_message)
 
-# Get the user's choice
-user_choice = input("Enter your choice (rock/paper/scissors): ").lower()
+# Get the user's choice - only first character necessary
+user_choice = input("Enter your choice - (r)ock/(p)aper/(s)cissors): ").lower()[0]
 
 # Send the user's choice to the server
 client_socket.sendall(user_choice.encode('utf-8'))
 
 # Receive the game result from the server
-result = client_socket.recv(1024)
+result = client_socket.recv(bufferSize)
 print(result.decode('utf-8'))
 
 # Close the connection
--- ServerSide.py
+++ ServerSide.py
@@ -1,10 +1,12 @@
 import socket
 
+bufferSize = 1024; # maximum size of transmission to listen for
+
 # Create a socket object
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Bind the socket to a specific address and port
-server_address = ('localhost', 12345)
+server_address = ('localhost', 50000)
 server_socket.bind(server_address)
 
 # Listen for incoming connections
@@ -16,19 +18,20 @@ player1_socket, player1_address = server_socket.accept()
 print('Player 1 connected:', player1_address)
 
 # Notify Player 1 to wait for Player 2
-player1_socket.sendall("Waiting for another player to connect...".encode('utf-8'))
+#sending this ends player1's game before they can see who won
+#player1_socket.sendall("Waiting for another player to connect...".encode('utf-8'))
 
 # Accept the second player's connection
 player2_socket, player2_address = server_socket.accept()
 print('Player 2 connected:', player2_address)
 
 # Notify both players that the game is starting
-player1_socket.sendall("Both players connected. The game is starting!".encode('utf-8'))
-player2_socket.sendall("Both players connected. The game is starting!".encode('utf-8'))
+player1_socket.sendall("Welcome, Player 1. Both players connected. The game is starting!".encode('utf-8'))
+player2_socket.sendall("Welcome, Player 2. Both players connected. The game is starting!".encode('utf-8'))
 
 # Receive choices from both players
-player1_choice = player1_socket.recv(1024).decode('utf-8')
-player2_choice = player2_socket.recv(1024).decode('utf-8')
+player1_choice = player1_socket.recv(bufferSize).decode('utf-8')
+player2_choice = player2_socket.recv(bufferSize).decode('utf-8')
 
 # Display the choices
 print('Player 1 chose:', player1_choice)
@@ -38,13 +41,15 @@ print('Player 2 chose:', player2_choice)
 if player1_choice == player2_choice:
     result = "It's a tie!"
 elif (
-    (player1_choice == 'rock' and player2_choice == 'scissors') or
-    (player1_choice == 'paper' and player2_choice == 'rock') or
-    (player1_choice == 'scissors' and player2_choice == 'paper')
+    (player1_choice == 'r' and player2_choice == 's') or
+    (player1_choice == 'p' and player2_choice == 'r') or
+    (player1_choice == 's' and player2_choice == 'p')
 ):
     result = "Player 1 wins!"
 else:
     result = "Player 2 wins!"
+
+print(result) # for the server
 
 # Send the result to both players
 player1_socket.sendall(result.encode('utf-8'))
